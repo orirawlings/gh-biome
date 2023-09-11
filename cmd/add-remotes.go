ed39f28cf07a7d9acf69da29857c0045b4d07830
Ori Rawlings <orirawlings@gmail.com>
2023-09-11 14:12:11 -0500

    add-remotes: Refactor to extract repo querying

--- cmd/add-remotes.go
+++ cmd/add-remotes.go
@@ -30,19 +30,9 @@ from any reference on the remote.`,
 	Args: cobra.MinimumNArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// load repos for given owners
-		remotes := make(remotes)
-		for _, hostAndOwner := range args {
-			hostAndOwner, _ = strings.CutPrefix(hostAndOwner, "http://")
-			hostAndOwner, _ = strings.CutPrefix(hostAndOwner, "https://")
-			parts := strings.SplitN(hostAndOwner, "/", 2)
-			host, owner := parts[0], parts[1]
-			rs, err := getRemotes(host, owner)
-			if err != nil {
-				return err
-			}
-			for name, r := range rs {
-				remotes[name] = r
-			}
+		repos, err := getRepos(args)
+		if err != nil {
+			return fmt.Errorf("could not query repositories: %w", err)
 		}
 
 		// persist remote data for git config editor
@@ -51,7 +41,7 @@ from any reference on the remote.`,
 			return fmt.Errorf("cannot create temporary file for remotes data: %w", err)
 		}
 		defer os.Remove(f.Name()) // clean up
-		if err := remotes.save(f); err != nil {
+		if err := newRemotes(repos).save(f); err != nil {
 			return fmt.Errorf("cannot save remotes data for git config editor: %w", err)
 		}
 		if err := f.Close(); err != nil {
@@ -75,6 +65,20 @@ type remote struct {
 }
 
 type remotes map[string]remote
+
+func newRemotes(repos []repository) remotes {
+	remotes := make(remotes)
+	for _, repo := range repos {
+		r := remote{
+			Name:     repo.URL[8:],
+			FetchURL: repo.URL + ".git",
+			Archived: repo.IsArchived,
+			Disabled: repo.IsLocked || repo.IsDisabled,
+		}
+		remotes[r.Name] = r
+	}
+	return remotes
+}
 
 func (rs remotes) save(w io.Writer) error {
 	var data struct {
@@ -102,54 +106,55 @@ func (rs *remotes) load(r io.Reader) error {
 	return nil
 }
 
-func getRemotes(host, owner string) (remotes, error) {
-	opts := api.ClientOptions{
-		Host: host,
-	}
-	client, err := api.NewGraphQLClient(opts)
-	if err != nil {
-		return nil, err
-	}
-	var query struct {
-		RepositoryOwner struct {
-			Repositories struct {
-				Nodes []struct {
-					IsDisabled bool
-					IsArchived bool
-					IsLocked   bool
-					URL        string `graphql:"url"`
-				}
-				PageInfo struct {
-					HasNextPage bool
-					EndCursor   string
-				}
-			} `graphql:"repositories(first: 100, after: $endCursor)"`
-		} `graphql:"repositoryOwner(login: $owner)"`
-	}
-	variables := map[string]interface{}{
-		"owner":     graphql.String(owner),
-		"endCursor": (*graphql.String)(nil),
-	}
-	page := 1
-	remotes := make(remotes)
-	for {
-		if err := client.Query("RepositoryOwner", &query, variables); err != nil {
-			return remotes, err
+type repository struct {
+	IsDisabled bool
+	IsArchived bool
+	IsLocked   bool
+	URL        string `graphql:"url"`
+}
+
+func getRepos(args []string) ([]repository, error) {
+	var repos []repository
+	for _, hostAndOwner := range args {
+		// parse host and owner
+		hostAndOwner, _ = strings.CutPrefix(hostAndOwner, "http://")
+		hostAndOwner, _ = strings.CutPrefix(hostAndOwner, "https://")
+		parts := strings.SplitN(hostAndOwner, "/", 2)
+		host, owner := parts[0], parts[1]
+
+		// query API
+		opts := api.ClientOptions{
+			Host: host,
 		}
-		for _, node := range query.RepositoryOwner.Repositories.Nodes {
-			r := remote{
-				Name:     node.URL[8:],
-				FetchURL: node.URL + ".git",
-				Archived: node.IsArchived,
-				Disabled: node.IsLocked || node.IsDisabled,
+		client, err := api.NewGraphQLClient(opts)
+		if err != nil {
+			return nil, fmt.Errorf("could not create API client: %s: %w", host, err)
+		}
+		var query struct {
+			RepositoryOwner struct {
+				Repositories struct {
+					Nodes    []repository
+					PageInfo struct {
+						HasNextPage bool
+						EndCursor   string
+					}
+				} `graphql:"repositories(first: 100, after: $endCursor)"`
+			} `graphql:"repositoryOwner(login: $owner)"`
+		}
+		variables := map[string]interface{}{
+			"owner":     graphql.String(owner),
+			"endCursor": (*graphql.String)(nil),
+		}
+		for {
+			if err := client.Query("RepositoryOwner", &query, variables); err != nil {
+				return repos, fmt.Errorf("could not query repos for %s/%s: %w", host, owner, err)
 			}
-			remotes[r.Name] = r
+			repos = append(repos, query.RepositoryOwner.Repositories.Nodes...)
+			if !query.RepositoryOwner.Repositories.PageInfo.HasNextPage {
+				break
+			}
+			variables["endCursor"] = graphql.String(query.RepositoryOwner.Repositories.PageInfo.EndCursor)
 		}
-		if !query.RepositoryOwner.Repositories.PageInfo.HasNextPage {
-			break
-		}
-		variables["endCursor"] = graphql.String(query.RepositoryOwner.Repositories.PageInfo.EndCursor)
-		page++
 	}
-	return remotes, nil
+	return repos, nil
 }
--- cmd/add-remotes_test.go
+++ cmd/add-remotes_test.go
@@ -37,6 +37,55 @@ func TestRemotes(t *testing.T) {
 	}
 }
 
+func TestNewRemotes(t *testing.T) {
+	built := newRemotes([]repository{
+		{
+			URL: "https://github.com/foo/bar",
+		},
+		{
+			IsArchived: true,
+			URL:        "https://github.com/foo/archived",
+		},
+		{
+			IsDisabled: true,
+			URL:        "https://github.com/foo/disabled",
+		},
+		{
+			IsLocked: true,
+			URL:      "https://github.com/foo/locked",
+		},
+	})
+	expected := byName([]remote{
+		{
+			Name:     "github.com/foo/bar",
+			FetchURL: "https://github.com/foo/bar.git",
+		},
+		{
+			Name:     "github.com/foo/archived",
+			FetchURL: "https://github.com/foo/archived.git",
+			Archived: true,
+		},
+		{
+			Name:     "github.com/foo/disabled",
+			FetchURL: "https://github.com/foo/disabled.git",
+			Disabled: true,
+		},
+		{
+			Name:     "github.com/foo/locked",
+			FetchURL: "https://github.com/foo/locked.git",
+			Disabled: true,
+		},
+	})
+	if len(expected) != len(built) {
+		t.Errorf("remote data has unexpected number of entries: expected %d, was %d", len(expected), len(built))
+	}
+	for name, r := range expected {
+		if r != built[name] {
+			t.Errorf("remote data entry for %s not as expected: expected %v, was %v", name, r, built[name])
+		}
+	}
+}
+
 func byName(remotes []remote) remotes {
 	result := make(map[string]remote)
 	for _, r := range remotes {
