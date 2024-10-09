38b029ac97130bbda1942557657d350d72ede021
Ori Rawlings <orirawlings@gmail.com>
2024-10-09 16:26:05 -0500

    feat(internal/config): Add Remote type

---
+++ internal/config/remote.go
@@ -0,0 +1,66 @@
+package config
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+// Remote represents a git remote in the biome configuration. Typically the
+// remote would be stored in git config, so references and objects can be
+// fetched from the remote. The biome configuration would also record metadata
+// about whether the remote repository is still active and fetchable in GitHub.
+type Remote struct {
+
+	// Name of the git remote in the biome repository.
+	Name string
+
+	// FetchURL to retrieve references and objects from.
+	FetchURL string
+
+	// Archived indicates that the remote repository is archived in GitHub,
+	// disabled from receiving new content.
+	// https://docs.github.com/en/repositories/archiving-a-github-repository
+	Archived bool
+
+	// Disabled indicates that the remote repository is locked in GitHub,
+	// disabled from any updates, usually because the repository has been
+	// migrated to a different git forge.
+	// https://docs.github.com/en/migrations/overview/about-locked-repositories
+	Disabled bool
+
+	// Head is the target git reference in the biome repository that this
+	// remote's symbolic HEAD reference should point to.
+	Head string
+}
+
+// FetchRefspec returns the refspec that should be used when fetching
+// references from the remote. The refspec will sync all references under
+// `refs/*` from the remote repo to `refs/remotes/<remote name>/*` in the
+// local repo. The destination part of the refspec is checked with
+// `git check-ref-format --refspec-pattern` to ensure it is valid.
+//
+// See https://git-scm.com/docs/git-check-ref-format
+func (r Remote) FetchRefspec() (string, error) {
+	src := "refs/*"
+	dst := fmt.Sprintf("refs/remotes/%s/*", r.Name)
+	c := exec.Command("git", "check-ref-format", "--refspec-pattern", dst)
+	if err := c.Run(); err != nil {
+		// TODO (orirawlings): Instead of failing here, ideally we could
+		// fallback to some alternate, normalized refspec value that we know
+		// will be valid. Anyone scripting around `git-for-each-ref` would need
+		// to understand what that normalized format looks like, so it can be
+		// handled properly. This would allow us to add-remotes even for
+		// repos whose names do not make valid refspec path components. For
+		// example, `.github` repos:
+		// https://docs.github.com/en/communities/setting-up-your-project-for-healthy-contributions/creating-a-default-community-health-file#supported-file-types
+		return "", fmt.Errorf("refspec pattern invalid: %q %w", dst, err)
+	}
+	return fmt.Sprintf("+%s:%s", src, dst), nil
+}
+
+// Supported returns true if this remote configuration is currently supported
+// by this tool. Unsupported remotes are skipped during configuration setup.
+func (r Remote) Supported() bool {
+	_, err := r.FetchRefspec()
+	return err == nil
+}
---
+++ internal/config/remote_test.go
@@ -0,0 +1,96 @@
+package config
+
+import (
+	"testing"
+)
+
+var (
+	barRemote = Remote{
+		Name:     "github.com/orirawlings/bar",
+		FetchURL: "https://github.com/orirawlings/bar.git",
+		Head:     "refs/remotes/github.com/orirawlings/bar/heads/main",
+	}
+	archivedRemote = Remote{
+		Name:     "github.com/orirawlings/archived",
+		FetchURL: "https://github.com/orirawlings/archived.git",
+		Archived: true,
+		Head:     "refs/remotes/github.com/orirawlings/archived/heads/master",
+	}
+	disabledRemote = Remote{
+		Name:     "github.com/orirawlings/disabled",
+		FetchURL: "https://github.com/orirawlings/disabled.git",
+		Disabled: true,
+		Head:     "refs/remotes/github.com/orirawlings/disabled/heads/main",
+	}
+	lockedRemote = Remote{
+		Name:     "github.com/orirawlings/locked",
+		FetchURL: "https://github.com/orirawlings/locked.git",
+		Disabled: true,
+		Head:     "refs/remotes/github.com/orirawlings/locked/heads/main",
+	}
+	headlessRemote = Remote{
+		Name:     "github.com/orirawlings/headless",
+		FetchURL: "https://github.com/orirawlings/headless.git",
+	}
+	dotPrefixRemote = Remote{
+		Name:     "github.com/orirawlings/.github",
+		FetchURL: "https://github.com/orirawlings/.github.git",
+		Head:     "refs/remotes/github.com/orirawlings/.github/heads/main",
+	}
+)
+
+func TestRemote_FetchRefspec(t *testing.T) {
+	// remotes with a vaild fetch refspec
+	for _, r := range []Remote{
+		barRemote,
+		archivedRemote,
+		disabledRemote,
+		lockedRemote,
+		headlessRemote,
+	} {
+		t.Run(r.Name, func(t *testing.T) {
+			_, err := r.FetchRefspec()
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+		})
+	}
+	// remotes with an invaild fetch refspec
+	for _, r := range []Remote{
+		dotPrefixRemote,
+	} {
+		t.Run(r.Name, func(t *testing.T) {
+			_, err := r.FetchRefspec()
+			if err == nil {
+				t.Errorf("expected error, but was nil")
+			}
+		})
+	}
+}
+
+func TestRemote_Supported(t *testing.T) {
+	// remotes with a vaild fetch refspec
+	for _, r := range []Remote{
+		barRemote,
+		archivedRemote,
+		disabledRemote,
+		lockedRemote,
+		headlessRemote,
+	} {
+		t.Run(r.Name, func(t *testing.T) {
+			if !r.Supported() {
+				t.Errorf("expected remote to be supported, but was not")
+			}
+		})
+	}
+	// remotes with an invaild fetch refspec
+	for _, r := range []Remote{
+		dotPrefixRemote,
+	} {
+		t.Run(r.Name, func(t *testing.T) {
+			if r.Supported() {
+				t.Errorf("expected remote to not be supported, but was")
+			}
+		})
+	}
+}
